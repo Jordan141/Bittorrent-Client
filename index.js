ac6145a76deb2aa52a2f3f5dffefa0b37115ba76
Jordan141 <jmoss636@gmail.com>
2018-08-26 22:51:13 +0100

    Minmal typo errors, linter fixes

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-const fs = require('fs')
-const bencode = require('bencode')
 const tracker = require('./tracker')
 const torrentParser = require('./torrent-parser')
 
 const torrent = torrentParser.open('puppy.torrent')
 
 tracker.getPeers(torrent, peers => {
-    console.log('list of peers', peers)
-})
+  console.log('list of peers: ', peers)
+});
--- util.js
+++ util.js
@@ -1,11 +1,11 @@
-const crypto = require('crypto')
+const crypto = require('crypto');
 
-let id = null
+let id = null;
 
 module.exports.genId = () => {
-    if(!id){
-        id = crypto.randomBytes(20)
-        Buffer.from('-AT0001-').copy(id, 0)
-    }
-    return id
-}
+  if (!id) {
+    id = crypto.randomBytes(20);
+    Buffer.from('-AT0001-').copy(id, 0);
+  }
+  return id;
+};
