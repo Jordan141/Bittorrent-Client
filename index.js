620ad58722961147a0899a28289efbd97bff378d
Jordan Moss <jmoss636@gmail.com>
2018-08-26 22:37:39 +0100

    Basic infrastructure

---
+++ index.js
@@ -0,0 +1,10 @@
+const fs = require('fs')
+const bencode = require('bencode')
+const tracker = require('./tracker')
+const torrentParser = require('./torrent-parser')
+
+const torrent = torrentParser.open('puppy.torrent')
+
+tracker.getPeers(torrent, peers => {
+    console.log('list of peers', peers)
+})
