c624fc0d075f6d9805e7133e6b37c5bfac6484d0
Jordan141 <jmoss636@gmail.com>
2018-08-28 01:22:42 +0100

    grouping messages

--- src/download.js
+++ src/download.js
@@ -17,3 +17,21 @@ function download(peer){
         //Handle response here later
     })
 }
+
+function onWholeMsg(socket, callback){
+    let savedBuf = Buffer.alloc(0)
+    let handshake = true
+
+    socket.on('data', revcBuf => {
+        const msgLen = () => handshake ? savedBuf.readUInt8(0) + 49 :
+        savedBuf.readInt32BE(0) + 4
+
+        savedBuf = Buffer.concat([savedBuf, revcBuf])
+
+        while(savedBuf.length >= 4 && savedBuf.length >= msgLen()){
+            callback(savedBuf.slice(0, msgLen()))
+            savedBuf = savedBuf.slice(msgLen())
+            handshake = false
+        }
+    })
+}
